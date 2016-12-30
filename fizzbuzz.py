543782ac88dad3c205b0323c2919639146617716
Zach Dischner <zach.dischner@colorado.edu>
2016-12-30 15:15:09 -0700

    Fixed switched FIZZ/BUZZ, added optional debug to printout, documentation for functions

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,18 @@
 #! /usr/local/bin/python
 
-"""Zach Dischner's submission to the modified fizz-buzz programming challenge
+"""
+Zach Dischner's submission to the modified fizz-buzz programming challenge
 listed at https://github.com/swift-nav/screening_questions/blob/master/questions.md#swift-navigation-application-questions
+
+Example:
+    ## Generate and fizz-buzz encode up to F[20] Fibonacci sequence numbers
+    python fizzbuzz.py 20
+
+    ## Perform `doctests` checks on simpler expressionable functionality
+    pytest --doctest-module fizzbuzz.py
+
+    ## Perform `pytest` checks on more involved "systems" functionality
+    pytest fizzbuzz.py
 
 """
 
@@ -26,6 +37,12 @@ def is_prime(x):
         Evens greator than 2 are not prime
         Otherwise check to see if any number up to sqrt(x) goes into x evenly
 
+    Note that under this definition, 0 and 1 are _NOT_ prime numbers. 0 has infinitely many
+    divisors, and 1 fails the logic of "a number is prime if it's only two divisors are 1
+    and itself". http://mathforum.org/library/drmath/view/57036.html. Negatives are also 
+    not considered primes, though that definition varies and an abs() pretty easily changes
+    this assumption.
+
     Args:
         x:  (integer) number to test for primeness
 
@@ -34,6 +51,8 @@ def is_prime(x):
 
     Examples:
         >>> is_prime(-1)
+        False
+        >>> is_prime(0)
         False
         >>> is_prime(1)
         False
@@ -67,6 +86,21 @@ def is_prime(x):
 
 def fib():
     """Memory efficient Fibonacci sequence generator
+
+    Note that the definition chosen here is that 0 is the first element in the Fibonacci sequence
+    F_0=0, F_1=1, F_2=1, F_3=2...
+    See https://en.wikipedia.org/wiki/Fibonacci_number
+
+    Returns:
+        _   (generator) Fibonacci number generator
+
+    Examples:
+        ## Generate some Fibonacci numbers
+        for ix,fn in enumerate(fizzbuzz.fib()):
+            print("Fib sequence number F({}) ==> {}".format(ix,fn))
+            if ix > 10:
+                break
+
     """
     one_ago, two_ago = 0,1
     while True:
@@ -83,24 +117,33 @@ def fizz_buzzify(x):
         * if x is divisible by 5 ==> "Buzz"
         * x ==> x otherwise
     
-    Note the order of precidence. 3 is prime as well as divisible by 3. But we return 
-    prime representation first. Logic follows that if prime didn't take precidence, then
-    x%15 wouldn't take precidence either and then we'd be stuck without ever having that
-    occurance and trying to figure out if "Fizz" or "Buzz" is the correct implementation. 
+    Note 1. Order of precidence: 
+        The number 3 is prime as well as divisible by 3. But here we choose to return 
+        prime representation first. Logic follows that if prime didn't take precidence, then
+        x%15 wouldn't take precidence either and then we'd be stuck without ever having that
+        occurance and trying to figure out if "Fizz" or "Buzz" is the correct implementation.
+
+    Note 2: 0 treatment
+        The number 0 is an odd one in that it is sometimes prime depending on your definition
+        (_not_ in this implementation though), and it is divisible by everything. So I'm keeping 
+        with program logic which says that 0 % 3 ==0 *and* 0 % 5 == 0 (which means 0 % 15 ==0)
+        so the fizzbuzzification of 0 will result in "FizzBuzz"
 
     Examples:
-    >>> fizz_buzzify(10)
-    'Buzz'
-    >>> fizz_buzzify(11)
-    'BuzzFizz'
-    >>> fizz_buzzify(12)
-    'Fizz'
-    >>> fizz_buzzify(15)
-    'FizzBuzz'
-    >>> fizz_buzzify(16)
-    16
+        >>> fizz_buzzify(0)  # divisible by 3 and 5
+        'FizzBuzz'
+        >>> fizz_buzzify(10) # divisible by 5
+        'Fizz'
+        >>> fizz_buzzify(11) # Prime
+        'BuzzFizz'
+        >>> fizz_buzzify(12) # Divisible by 3
+        'Buzz'
+        >>> fizz_buzzify(15) # Divisible by 3 and 5
+        'FizzBuzz'
+        >>> fizz_buzzify(16) # Not prime, not divisible by 3 or 5
+        16
     """
-    ## First test for primeness
+    ## First test for primeness  
     if is_prime(x):
         return "BuzzFizz"
     
@@ -108,21 +151,50 @@ def fizz_buzzify(x):
     divis_3 = ""
     divis_5 = ""
     if (x % 3) == 0:
-        divis_3 += "Fizz"
+        divis_3 += "Buzz"
     if (x % 5) == 0:
-        divis_5 += "Buzz"
+        divis_5 += "Fizz"
 
     ## Assemble fizzbuzzed string or return the number. handy one liner
-    return (divis_3 + divis_5) or x
+    # Note that anything divisible by 3 and 5 is also divisable by 15 (this is how we get "FizzBuzz")
+    return (divis_5 + divis_3) or x
 
 
-def generate_fizz_buzz(N):
-    """Generates a fizz-buzzed Fibonacci Sequence of length N
+def generate_fizz_buzz(N, debug=False):
+    """Generates a fizz-buzzed Fibonacci Sequence up to Fibonacci number N
+
+    Args:
+        N:  (int) Cap on Fibonacci sequence number to generate
+    Kwargs:
+        debug:  (bool) Print out more comprehensive fizzbuzzification, helpful for debugging
+                    and inspecting behavior yourself.
+
+    Examples:
+        In[X]: generate_fizz_buzz(5)
+        ... prints out: 
+                FizzBuzz
+                1
+                1
+                BuzzFizz
+                BuzzFizz
+                BuzzFizz
+        In[X]: generate_fizz_buzz(5, debug=True)
+        ... prints out: 
+                F[0] ==> 0 ==> FizzBuzz
+                F[1] ==> 1 ==> 1
+                F[2] ==> 1 ==> 1
+                F[3] ==> 2 ==> BuzzFizz
+                F[4] ==> 3 ==> BuzzFizz
+                F[5] ==> 5 ==> BuzzFizz
     """
     for index, fib_number in enumerate(fib()):
         if index > N:
             break
-        print(fizz_buzzify(fib_number))
+        fizzbuzzed = fizz_buzzify(fib_number)
+        if debug:
+            print("F[{}] ==> {} ==> {}".format(index, fib_number, fizzbuzzed))
+        else:
+            print(fizzbuzzed)
 
 
 
@@ -133,7 +205,7 @@ def test_fib():
     fibgen = fib()
     fib5 = [next(fibgen) for ix in range(5)]
     truth = [0,1,1,2,3]
-    assert fib5==truth, "Fibonacci sequence generator failed, {} should == {} but it doesnt".format(fib5,truth)
+    assert fib5==truth, "Fibonacci sequence generator failed, {} should == {}".format(fib5,truth)
 
 ##############################################################################
 #                                  Runtime
@@ -143,12 +215,14 @@ if __name__ == "__main__":
     parser   = argparse.ArgumentParser(description='Fizz-Buzzify a Fibonacci sequence',
                     epilog='Example of use: python fizzbuzz.py 5')
     parser.add_argument('N', type=int, help="Length of Fibonacci Sequence to produce")
+    parser.add_argument('--debug',action='store_true', help="Printout extra info illustrating the fizzbuzzification")
     args = parser.parse_args()
     N = args.N
+    debug = args.debug
 
     ###### Generate Fizz Buzz'd Fibonacci Sequence
     print("Generating a Fibonacci sequence of length {N} for fizz-buzzifying".format(N=N))
-    generate_fizz_buzz(N)
+    generate_fizz_buzz(N,debug=debug)
     sys.exit(0)
 
 
